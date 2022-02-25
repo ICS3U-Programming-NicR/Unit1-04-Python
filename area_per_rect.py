098a09165cb64179d96431c6a4252a6994a0185c
nictheriscer <nicolas.riscalas@stu.ocsb.ca>
2022-02-25 14:14:49 +0000

    gfifda

--- area_per_rect.py
+++ area_per_rect.py
@@ -9,8 +9,8 @@
 
 def main():
     print("If a rectangle has the dimensions:\n7cm x 8cm")
-    print("The area is {}cm^2".format(7*8))
-    print("The perimeter is {}cm".format(2*(7+8)))
+    print("The area is {}cm^2".format(7 * 8))
+    print("The perimeter is {}cm".format(2 * (7 + 8)))
 
 
 if __name__ == "__main__":
